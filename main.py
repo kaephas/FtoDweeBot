1df7044e731b3c4ec79fe9a0a486362df887010a
Kaephas Kain <kaephas@gmail.com>
2021-02-20 19:38:48 +0000

    another change to try to update repo

--- main.py
+++ main.py
@@ -33,6 +33,6 @@ async def on_message(message):
         unit = "Celsius"
       await message.channel.send(str(round(convert, 2)) + " " + unit);
     else:
-      await message.channel.send("Usage: $dwee # [-c (for C to F)]")
+      await message.channel.send("Usage: $dwee # (add -c for C to F")
 
 client.run(os.getenv('TOKEN'))
